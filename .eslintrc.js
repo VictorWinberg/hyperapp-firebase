44bfa4a99f4480f2dd49dc717142ef9ebf4e1d59
Victor Winberg <victor.winberg@jayway.com>
2020-10-17 12:29:01 +0200

    Add husky

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,13 +3,13 @@ module.exports = {
   extends: [
     'standard',
     'plugin:prettier/recommended',
-    'plugin:jsx-a11y/recommended'
+    'plugin:jsx-a11y/recommended',
   ],
   plugins: ['import', 'react', 'prettier', 'jsx-a11y'],
   rules: {
     'no-unused-vars': [2, { varsIgnorePattern: 'React|jsx|h' }],
     'react/jsx-uses-vars': 2,
     'no-undef': 0,
-    'import/no-absolute-path': 0
-  }
-}
+    'import/no-absolute-path': 0,
+  },
+};
